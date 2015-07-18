45a535f5d0ae819d374757caac67993510ec9265
Andrew Martin <amartin@avidandrew.com>
2015-07-17 21:43:18 -0500

    reduced width of VNC iframe to fit VNC session

--- templates/config.php
+++ templates/config.php
@@ -4,7 +4,7 @@
 $APP = "Web Launcher";
 $VIEWER_HOST = "{{ viewer_hostname }}";
 $DOWNLOAD_LINK = "{{ apk_url }}";
-$VIEWER_VNC_WIDTH = 825;
+$VIEWER_VNC_WIDTH = 810;
 $VIEWER_VNC_HEIGHT = 635;
 
 // list links below, one per line; format is "display text" => "http://thelink.com",
